75ebb399f5719ff97068e8ddbf39ac9f74104c10
Olga Varaksa <48439524+OlgaVaraksa@users.noreply.github.com>
2020-05-29 14:02:58 +0300

    bugfiks

--- widgets/BlogAllArticlesPage.php
+++ widgets/BlogAllArticlesPage.php
@@ -324,6 +324,9 @@ class BlogAllArticlesPage extends Widget_Base
                                 $pageId = $categoryPage->ID;
                                 ?>
                                 <div class="columns-blog">
+
+
+                                    <a href="<?php echo get_page_link($pageId); ?>">
                                     <div class="blog-card card">
                                         <div class="blog-card-thumb">
                                             <img src="<?php echo get_the_post_thumbnail_url($categoryPage->ID, 'large'); ?>">
@@ -335,12 +338,16 @@ class BlogAllArticlesPage extends Widget_Base
                                             <div class="blog-card-excerpt">
                                                 <?php echo $categoryPage->post_title; ?>
                                             </div>
-                                            <a href="<?php echo get_page_link($pageId); ?>" class="blog-card-link">
+                                            <div class="blog-card-link">
                                                 Read More
                                                 <img src="https://cdn.permission.io/apps/permissionbase/assets/icons/chevron-right.svg">
-                                            </a>
+                                            </div>
                                         </div>
                                     </div>
+                                    </a>
+
+
+
                                 </div>
                             <?php } ?>
                         </div>
